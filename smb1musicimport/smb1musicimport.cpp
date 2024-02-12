b50d0f62637955ab92e6f90694c63af8b3d4d2fc
studsX <zipzipchess@gmail.com>
2024-02-12 08:42:18 +0100

    musiclengthlookuptbl fix

--- smb1musicimport/smb1musicimport.cpp
+++ smb1musicimport/smb1musicimport.cpp
@@ -2,10 +2,10 @@
 #include <format>
 #include "famitracker_parser.h"
 
-#define SP(speed) speed*1, speed*2, speed*3, speed*4, speed*6, speed*8, speed*12, speed*16
+#define SP(speed) (speed)*1, (speed)*2, (speed)*3, (speed)*4, (speed)*6, (speed)*8, (speed)*12, (speed)*16
 #define tenSP(of) SP(of), SP(of+1), SP(of+2), SP(of+3), SP(of+4), SP(of+5), SP(of+6), SP(of+7), SP(of+8), SP(of+9)
 
-void write_to_file(std::ofstream& file, uint8_t array[], int size, int mod = 7)
+void write_to_file(std::ofstream& file, uint8_t array[], int size, int mod = 8)
 {
     file << "\t.db ";
     for (int i = 0; i < size; i++)
@@ -34,9 +34,9 @@ int main()
     ofile << "MusicLengthLookupTbl:\n";
     uint8_t MusicLengthLookupTbl[] =
     {
-        tenSP(1), tenSP(11), tenSP(21), SP(31), SP(32)
+        tenSP(1), SP(11), SP(12), SP(13), SP(14), SP(15)
     };
-    write_to_file(ofile, MusicLengthLookupTbl, 256);
+    write_to_file(ofile, MusicLengthLookupTbl, 120);
 
     file.select_track(2);
     std::vector<std::vector<uint8_t>> music_data(4);
