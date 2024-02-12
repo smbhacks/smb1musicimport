02544890c99c578d6cb612a8cbbc35f76259a58c
studsX <zipzipchess@gmail.com>
2024-02-12 13:30:49 +0100

    2 and 3 based rows

--- smb1musicimport/smb1musicimport.cpp
+++ smb1musicimport/smb1musicimport.cpp
@@ -18,7 +18,9 @@ uint8_t MusicLengthLookupTbl[] =
     SP_2(1), SP_2(2), SP_2(3), SP_2(4), SP_2(5), SP_2(6), SP_2(7),
     SP_3(1), SP_3(2), SP_3(3), SP_3(4), SP_3(5), SP_3(6), SP_3(7)
 };
-
+uint8_t RowsBase2[8] = { SP_2(1) };
+uint8_t RowsBase3[8] = { SP_3(1) };
+uint8_t UsedRowSizes[8] = { SP_2(1) };
 uint8_t handle_sq2_note(std::string pitch_table, std::string note)
 {
     int p = pitch_table.find(note);
@@ -39,9 +41,9 @@ bool handle_sq2_rhythm(std::vector<uint8_t>& data, int row_value, std::string pi
     {
         for (int x = 7; x >= 0; x--)
         {
-            if (row_value - MusicLengthLookupTbl[x] >= 0)
+            if (row_value - UsedRowSizes[x] >= 0)
             {
-                row_value -= MusicLengthLookupTbl[x];
+                row_value -= UsedRowSizes[x];
                 data.push_back(0x80 + x);
                 if (put_down_note) data.push_back(handle_sq2_note(pitch_table, "..."));
                 else if(row_value != 0)
@@ -78,16 +80,54 @@ int main()
     ofile << "MusicLengthLookupTbl:\n";
     write_to_file(ofile, MusicLengthLookupTbl, 112);
 
-    file.select_track(2);
     std::vector<std::vector<std::vector<uint8_t>>> music_data(4);
     std::fill(music_data.begin(), music_data.end(), std::vector<std::vector<uint8_t>>(256));
 
-    //parse SQ2 and TRI
     const char* channel_names[] = { "SQ1_CH", "SQ2_CH", "TRI_CH", "NOI_CH" };
     const int SQ1_CH = 0;
     const int SQ2_CH = 1;
     const int TRI_CH = 2;
     const int NOI_CH = 3;
+
+    //calculate whether to use 2 or 3 based music by
+    //counting the row lengths
+    int div2 = 0;
+    int div3 = 0;
+    file.select_track(2);
+    for (int ch = 0; ch < 4; ch++)
+    {
+        for (int order_no = 0; order_no < file.num_of_orders; order_no++)
+        {
+            if (file.already_did_pattern(ch, order_no)) continue;
+            file.go_to_pattern(ch, order_no);
+
+            bool d00_effect = false;
+            while (!file.end_of_pattern() && !d00_effect)
+            {
+                int next_note_distance = 0;
+                std::string check_note;
+                do
+                {
+                    std::vector<std::string> effects = file.get_effects(1, 0);
+                    check_note = file.get_note(0, 0);
+                    file.current_row();
+                    d00_effect = std::find(effects.begin(), effects.end(), "D00") != effects.end();
+                    next_note_distance++;
+                } while (check_note == "..." && !d00_effect);
+                if (next_note_distance % 2 == 0) div2++;
+                if (next_note_distance % 3 == 0) div3++;
+            }
+            file.pattern_done(ch, order_no);
+        }
+    }
+    //if 3 based song, overwrite the row sizes
+    if (div3 > div2)
+    {
+        for (int i = 0; i < 8; i++) UsedRowSizes[i] = RowsBase3[i];
+    }
+
+    //handle actual parsing
+    file.select_track(2);
     for (int ch = 0; ch < 4; ch++)
     {
         for (int order_no = 0; order_no < file.num_of_orders; order_no++)
@@ -130,6 +170,5 @@ int main()
 
             file.pattern_done(ch, order_no);
         }
-        std::cout << std::endl;
     }
 }
